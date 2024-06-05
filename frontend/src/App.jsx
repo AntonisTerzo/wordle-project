cbe159946506daea7884843652292d0f4aa8d0e5
AntonisTerzo <antonis.terzopoulos@gmail.com>
2024-06-05 10:11:08 +0200

    Merge branch 'main' of https://github.com/AntonisTerzo/wordle-project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-
-
   return (
-    <h1 className='text-center'>Wordle</h1>
-  )
+    <nav className='h-16 w-full m-0 border-b grid-cols-* justify-center'>
+      <h1 className='text-center text-red-800 font-bold'>Wordle</h1>
+    </nav>
+  );
 }
 
-export default App
+export default App;
