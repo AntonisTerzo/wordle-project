a31f05aa9f1976fa4a6b7af7ada5d80aff04d8be
AntonisTerzo <antonis.terzopoulos@gmail.com>
2024-06-05 13:20:29 +0200

    created the about api and installed handlebars

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,13 @@
-import express from "express";
+import express from 'express';
+import { engine } from 'express-handlebars';
 
 const app = express();
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+app.get('/about', (req, res) => {
+  res.render('about');
+});
 
 export default app;
