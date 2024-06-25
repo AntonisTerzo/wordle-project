33ed3fdbaf8033a5d080603edaf088b74f093336
AntonisTerzo <antonis.terzopoulos@gmail.com>
2024-06-25 11:43:02 +0200

    made a change in randomword algo and some minor changes in the model for db

--- backend/src/models.js
+++ backend/src/models.js
@@ -1,12 +1,14 @@
 import mongoose from 'mongoose';
 
 const playerScore = mongoose.model('playerScore', {
-    correctWord: String,
-    wordLength: Number,
-    includeDuplicates: Boolean,
+    gameId: String,
+    chosenWord: String,
+    startTime: Number,
     guesses: Number,
-    duration: Number,
+    endTime: Number,
+    includeDuplicates: Boolean,
     playerName: String,
+    duration: Number,
 });
 
 export { playerScore };
