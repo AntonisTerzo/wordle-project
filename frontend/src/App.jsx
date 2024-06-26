a92b561388b29857dcd9356534fef0940e43e29e
AntonisTerzo <antonis.terzopoulos@gmail.com>
2024-06-26 17:45:39 +0200

    created sendscore component and finished the app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,133 @@
-import { useState } from 'react';
-import './App.css';
+import { useState, useEffect, useRef } from "react";
+import GuessWord from "./components/GuessWord"; 
+import SetDifficulty from "./components/SetDifficulty";
+import SendScore from "./components/SendScore";
+import "./scss/app.scss";
+import Navbar from "./components/Navbar";
 
 function App() {
+  const [results, setResults] = useState([]);
+  const [startGame, setStartGame] = useState(false);
+  const [win, setWin] = useState(false);
+  const [wordLength, setWordLength] = useState();
+  const [score, setScore] = useState()
+  const [errorMessage, setErrorMessage] = useState();
+  const [gameId, setGameId] = useState();
+  const [posted, setPosted] = useState(false);
+  const timerId = useRef(null);
+
+  useEffect(() => {
+    if (errorMessage) {
+      timerId.current = setTimeout(() => {
+        setErrorMessage()
+      }, 3000)
+    }
+    return () => {
+      clearTimeout(timerId.current)
+    };
+  }, [errorMessage])
+
+  async function handleStartGame(chosenDifficulty) {
+    const response = await fetch("/api/new_game", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(chosenDifficulty),
+    });
+
+    if(response.ok){
+    setStartGame(!startGame);
+    setWordLength(chosenDifficulty.length);
+    const id = await response.json();
+    setGameId(id);
+    }else {
+      handleError('No word found')
+    }
+  }
+
+  async function handleGuess(newGuess) {
+    if (!newGuess) {
+      return
+    }
+
+    const response = await fetch(`/api/${gameId}/guesses`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ newGuess }),
+    });
+
+    if(response.ok){
+    const data = await response.json();
+
+    if (!data[0].length) {
+      return
+    }
+
+    const updatedresults = [
+      ...results,
+      data[0]
+    ]
+
+    setResults(updatedresults)
+
+    if (data[1]) {
+      setScore(data[1])
+      setWin(!win)
+    }
+  }else {
+    const data = await response.json();
+    handleError(data);
+  }
+  }
+
+  async function handleScore(playerName) {
+    const response = await fetch(`/api/${gameId}/highscores`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ playerName }),
+    });
+    if(!response.ok){
+      handleError(await response.json())
+    }else{
+      setPosted(true)
+    }
+  }
+
+  function handleAbort() {
+    setStartGame(!startGame)
+    setWin(false)
+    setResults([])
+    setScore();
+    setGameId();
+    setPosted(false)
+  }
+
+  function handleError(message) {
+    setErrorMessage(message)
+  }
+
   return (
-    <nav className='h-16 w-full m-0 border-b grid-cols-* justify-center'>
-      <h1 className='text-center text-red-800 font-bold'>Wordle</h1>
-    </nav>
+    <>
+      <div className="wordleGame">
+      <Navbar />
+        <div className="wordleGame__wrapper">
+        {win && < SendScore onPostScore={handleScore} score={score} abort={handleAbort} posted={posted} onError={handleError} errorMessage={errorMessage} />}
+          {!startGame}
+          <h2 className="wordleGame__title">
+            {!startGame ? "Game Settings" : "Guess the word!"}
+          </h2>
+          <hr className="wordleGame__line" />
+          {!startGame && < SetDifficulty onStartGame={handleStartGame} error={handleError} />}
+          {startGame && < GuessWord onGuess={handleGuess} maxInput={wordLength} abort={handleAbort} results={results} error={handleError} />}
+          {errorMessage && <p className="error">{errorMessage}</p>}
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ frontend/src/components/SendScore.jsx
@@ -0,0 +1,82 @@
+import { useState } from 'react';
+
+export default function SendScore({
+  onPostScore,
+  score,
+  posted,
+  onError,
+  errorMessage,
+  abort,
+}) {
+  const [player, setPlayer] = useState('');
+
+  return (
+    <div className='sendScore__wrapper'>
+      {posted && (
+        <>
+          <h2 className='sendScore__title'>Score posted!</h2>
+        </>
+      )}
+      {!posted && (
+        <>
+          <h2 className='sendScore__title'>You did it!</h2>
+          <h2 className='sendScore__title--small'>
+            Post you'r score and/or start a new game
+          </h2>
+          <hr className='wordleGame__line' />
+          <ul className='sendScore__score'>
+            <li className='sendScore__scoreItem'>
+              {`Chosen Word: ${score.chosenWord.toUpperCase()}`}
+            </li>
+            <li className='sendScore__scoreItem'>
+              {`Length of word: ${score.chosenWord.length}`}
+            </li>
+            <li className='sendScore__scoreItem'>
+              {`Guesses: ${score.guesses.length}`}
+            </li>
+            <li className='sendScore__scoreItem'>
+              {`Time: ${(score.endTime - score.startTime) / 1000}.sec`}
+            </li>
+            <li className='sendScore__scoreItem'>
+              {`Duplicate letters: ${score.includeDuplicates}`}
+            </li>
+          </ul>
+          <form
+            action=''
+            className='sendScore__post'
+            onSubmit={(e) => {
+              e.preventDefault();
+              if (!player.length) {
+                onError('Please insert a name before posting');
+                return;
+              }
+              onPostScore(player);
+            }}
+          >
+            <input
+              className='sendScore__postInput'
+              placeholder='Write your name'
+              type='text'
+              maxLength={10}
+              onChange={(e) => {
+                setPlayer(e.target.value);
+              }}
+            />
+            <button className='sendScore__postSubmit' type='submit'>
+              Post Score
+            </button>
+          </form>
+        </>
+      )}
+      {errorMessage && <p className='error'>{errorMessage}</p>}
+      <a
+        className='sendScore__newGame'
+        onClick={(e) => {
+          abort();
+        }}
+      >
+        New game
+      </a>
+    </div>
+  );
+}
