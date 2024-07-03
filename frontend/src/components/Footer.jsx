3b4d104bbf740a0812cfce3615680d89a27d40cb
AntonisTerzo <antonis.terzopoulos@gmail.com>
2024-07-03 14:59:09 +0200

    made changes so that the frontend is run by a cdn

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,11 @@
+import superMarioImg from '../../public/superMario.png'
+
 export default function Footer() {
   return (
     <div className='footer'>
       <p className='superMario'>
         Theme Inspired by Super Mario
-        <img className='superMario__img' src='/superMario.png' />
+        <img className='superMario__img' alt='A super mario image' src={superMarioImg} />
       </p>
     </div>
   )
